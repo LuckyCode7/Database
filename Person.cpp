ff6f8da5473fae36c9887dfd1ab8cc0f9cf3f2cd
Rafał Nowak <nowak9.private@gmail.com>
2018-07-26 18:23:40 +0200

    Added new methods

--- Person.cpp
+++ Person.cpp
@@ -1,2 +1,122 @@
 #include "Person.hpp"
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+bool Person::peselValidation(const std::array<int, 11> p)
+{
+    if ((9 * p[0] + 7 * p[1] + 3 * p[2] + 1 * p[3] + 9 * p[4] + 7 * p[5] + 3 * p[6] + 1 * p[7] + 9 * p[8] + 7 * p[9]) % 10 == p[10])
+        return true;
+    return false;
+}
+
+Person::~Person() {}
+
+void Person::setPESEL(const std::array<int, 11> pesel)
+{
+    this->PESEL = pesel;
+    int sign;
+    while (!peselValidation(this->PESEL))
+    {
+        cout << "Set correct PESEL: ";
+        cout << "-----------\b\b\b\b\b\b\b\b\b\b\b";
+        for (int i = 0; i < 11; i++)
+        {
+            sign = _getch();
+            switch (sign)
+            {
+            case 48:
+                PESEL[i] = 0;
+                cout << 0;
+                break;
+            case 49:
+                PESEL[i] = 1;
+                cout << 1;
+                break;
+            case 50:
+                PESEL[i] = 2;
+                cout << 2;
+                break;
+            case 51:
+                PESEL[i] = 3;
+                cout << 3;
+                break;
+            case 52:
+                PESEL[i] = 4;
+                cout << 4;
+                break;
+            case 53:
+                PESEL[i] = 5;
+                cout << 5;
+                break;
+            case 54:
+                PESEL[i] = 6;
+                cout << 6;
+                break;
+            case 55:
+                PESEL[i] = 7;
+                cout << 7;
+                break;
+            case 56:
+                PESEL[i] = 8;
+                cout << 8;
+                break;
+            case 57:
+                PESEL[i] = 9;
+                cout << 9;
+                break;
+            default:
+                --i;
+                cout << "\a";
+                break;
+            }
+        }
+    }
+    cout << endl;
+}
+
+void Person::setGender(const string& gender_)
+{
+    if (gender_ != "man" && gender_ != "woman")
+    {
+        cout << "Incorrect type of gender" << endl;
+        do
+        {
+            cout << "Set gender <man/woman> :";
+            cin >> this->gender;
+        } while (this->gender != "man" && this->gender != "woman");
+    }
+    else
+        this->gender = gender_;
+}
+
+void Person::setFirstName(const string& firstName_)
+{
+    this->firstName = firstName_;
+    this->firstName[0] = toupper(this->firstName[0]);
+}
+
+void Person::setLastName(const string & LastName_)
+{
+    this->lastName = LastName_;
+    this->lastName[0] = toupper(this->lastName[0]);
+}
+
+void Person::setAdress()
+{
+    string city;
+    string street;
+    cout << "Set city: ";
+    cin >> city;
+    cout << "Set street: ";
+    cin >> street;
+    city[0] = toupper(city[0]);
+    street[0] = toupper(street[0]);
+    this->adress = city + " ," + street + " street";
+    cout << this->adress;
+}
+
+
 
--- Person.hpp
+++ Person.hpp
@@ -1,2 +1,28 @@
 #pragma once
+#include <string>
+#include <iostream>
+#include <array>
+#include <stdlib.h>
+#include <conio.h>
+#include <cctype>
 
+using std::string;
+
+class Person
+{
+private:
+    string firstName;
+    string lastName;
+    std::array<int, 11> PESEL;
+    string gender;
+    string adress;
+    bool peselValidation(const std::array<int, 11>);
+public:
+    Person() {}
+    virtual ~Person();
+    void setPESEL(const std::array<int, 11>);
+    void setGender(const string&);
+    void setFirstName(const string&);
+    void setLastName(const string&);
+    void setAdress();
+};
