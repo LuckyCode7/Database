7d370c2d8de4f54d4deeb228b22d2441defba76b
Rafał Nowak <nowak9.private@gmail.com>
2018-07-28 18:16:45 +0200

    const int& instead od int

--- Employee.hpp
+++ Employee.hpp
@@ -7,7 +7,7 @@ private:
     int salary;
 public:
     Employee(const string& firstName_, const string& lastName_, const std::array<int, 11>& pesel,
-        const string& gender, const string& adress, int salary);
+        const string& gender, const string& adress, const int& salary);
     ~Employee();
     void setSalary(const int&);
     void showPerson();
