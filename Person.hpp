c32b529d5888dfaf95079bbb38ae0b25ed197e25
Rafał Nowak <nowak9.private@gmail.com>
2018-07-28 16:16:36 +0200

    Added virtual method setIndex

--- Person.hpp
+++ Person.hpp
@@ -7,28 +7,35 @@
 #include <cctype>
 #include <sstream>
 #include <limits>
+#include "Exceptions.hpp"
+
+using std::cin;
+using std::cout;
+using std::endl;
 
 using std::string;
 
 class Person
 {
-private:
+protected:
     string firstName;
     string lastName;
     std::array<int, 11> PESEL;
     string gender;
     string adress;
+private:
     bool checkPesel(const std::array<int, 11>);
     bool checkName(const string&);
     bool checkAdress(const string&);
     bool checkGender(const string&);
     void showPesel();
 public:
-    virtual ~Person() = 0;
+    virtual ~Person() {}
     void setPesel(const std::array<int, 11>);
     void setGender(const string&);
     void setFirstName(const string&);
     void setLastName(const string&);
     void setAdress(const string&);
     virtual void showPerson();
+    virtual void setIndex(const int&) = 0;
 };
