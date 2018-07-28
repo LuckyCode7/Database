0cba51bc7fd4d7c6f1f290276ec01be4e367067b
Rafał Nowak <nowak9.private@gmail.com>
2018-07-28 17:01:01 +0200

    Added new class: Employee

---
+++ Employee.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include "Person.hpp"
+
+class Employee : public Person
+{
+private:
+    int salary;
+public:
+    Employee(const string& firstName_, const string& lastName_, const std::array<int, 11>& pesel,
+        const string& gender, const string& adress, int salary);
+    ~Employee();
+    void setSalary(const int&);
+    void showPerson();
+};
