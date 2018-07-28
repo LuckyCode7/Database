01b50142c8e3cef74670b1fa13de82ff7fb13118
Rafał Nowak <nowak9.private@gmail.com>
2018-07-28 16:18:04 +0200

    Added methods: correctIndex, setIndex and showPerson

--- Student.hpp
+++ Student.hpp
@@ -5,9 +5,11 @@ class Student : public Person
 {
 private:
     int index;
+    bool correctIndex(const int&);
 public:
     Student(const string& firstName_, const string& lastName_, const std::array<int, 11>& pesel,
         const string& gender, const string& adress, int index);
     ~Student();
-
+    void setIndex(const int&);
+    void showPerson();
 };
