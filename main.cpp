715f1ebb9ffe7db7090e328b7da61be8a9e9ac54
Rafał Nowak <nowak9.private@gmail.com>
2018-08-01 11:53:21 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,25 +7,20 @@ using namespace std;
 int main()
 {
     Database database;
-    array<int, 11> pesel1 = { 4,4,0,5,1,4,0,1,3,5,9 };
-    array<int, 11> pesel2 = { 9,3,0,1,1,3,9,7,0,1,4 };
-    array<int, 11> pesel3 = { 2,5,0,4,0,7,5,1,9,1,0 };
-    array<int, 11> pesel4 = { 0,0,3,0,2,5,5,7,2,0,2 };
-    array<int, 11> pesel5 = { 9,1,0,5,1,9,6,2,0,1,2 };
-
-    Student student("rafal", "nowak", pesel2, "man", "warszawa, 15 cicha", 123456);
+   
+    Student student("rafal", "nowak", "93011397014", "man", "warszawa, 15 cicha", 123456);
     Person* person1 = &student;
 
-    Student student2("Jan", "tyski", pesel3, "man", "warszawa, 7 cicha", 227401);
+    Student student2("Jan", "tyski", "25040751910", "man", "warszawa, 7 cicha", 227401);
     Person* person3 = &student2;
    
-    Employee employee("ula", "kowlaksa", pesel1, "woman", "kalisz, 4 grunwaldzka", 3500);
+    Employee employee("ula", "kowlaksa", "44051401359", "woman", "kalisz, 4 grunwaldzka", 3500);
     Person* person2 = &employee;
 
-    Employee employee2("michal", "maly", pesel4, "man", "poznan, 90 spokojna", 5500);
+    Employee employee2("michal", "maly", "00302557202", "man", "poznan, 90 spokojna", 5500);
     Person* person4 = &employee2;
 
-    Employee employee3("marcin", "duzy", pesel5, "man", "krakow, 90 spokojna", 3000);
+    Employee employee3("marcin", "duzy", "91051962012", "man", "krakow, 90 spokojna", 3000);
     Person* person5 = &employee3;
 
     database.addToDatabase(person1);
@@ -37,13 +32,13 @@ int main()
     //database.showDatabase();
 
     //database.findLastName("Nowak");
-    //database.findPesel(pesel1);
+    //database.findPesel("9301139701");
 
     //database.sortByLastName();
-   // database.showDatabase();
-
-   // database.sortByPesel();
     //database.showDatabase();
+
+     //database.sortByPesel();
+     //database.showDatabase();
 
     database.sortBySalary();
     database.showDatabase();
