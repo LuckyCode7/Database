feae617139365cf735d02c535c124eade3c5572f
Rafał <40712810+LuckyCode7@users.noreply.github.com>
2019-02-03 23:03:31 +0100

    Update AdressValidator.hpp

--- inc/AdressValidator.hpp
+++ inc/AdressValidator.hpp
@@ -7,7 +7,7 @@ class AdressValidator : public DataValidator
 private:
     static bool checkCityAndStreetLetters(const Adress& adress) noexcept;
     static bool checkCityAndStreetSize(const Adress& adress) noexcept;
-     static bool checkNumberOfStreet(const Adress& adress) noexcept;
-  public:
+    static bool checkNumberOfStreet(const Adress& adress) noexcept;
+public:
     static bool checkAdress(const Adress& adress)  noexcept;
 };
